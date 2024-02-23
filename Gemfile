a26c7f1e2512973a0d782d3132f244d8ca5a8bd2
nikhil200044 <nikhil200044@gmail.com>
2024-02-23 15:29:31 +0530

    Configure Active admin

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem "bootsnap", require: false
 
 gem 'activeadmin' # For Admin Functionalities
 gem 'devise' # For user authentication
+gem 'sassc-rails', '~> 2.0' # For Load SCSS Files
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user_or_admin!
+
+  private
+
+  def authenticate_user_or_admin!
+    if request.path.starts_with?('/admin') # Check if the request is for the admin panel
+      authenticate_admin_user!
+    else
+      authenticate_user!
+    end
+  end
 end
