f149d93ef0d680f9b96c00c9c1e5a2110856cf22
nikhil200044 <nikhil200044@gmail.com>
2024-02-26 22:45:20 +0530

    Added Thumbnail ad a attachment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,11 @@
 class Course < ApplicationRecord
+  has_one_attached :thumbnail
+
+  def self.ransackable_attributes(auth_object = nil)
+    super
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    # ['thumbnail_attachment', 'thumbnail_blob', "blob_id", "created_at", "id", "name", "record_id", "record_type"]
+  end
 end
