132b2885ab7c7f651c05b0e259dbea4f4d92763f
nikhil200044 <nikhil200044@gmail.com>
2024-02-27 11:35:57 +0530

    Allow render hosts

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module CourseCatalog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << ".onrender.com"
   end
 end
